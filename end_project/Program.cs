a5914bb70f00693024fc7bc1ff0a52e3b313afc6
Denis <den.batuev99@gmail.com>
2022-08-09 23:22:41 +0300

    second commit

--- end_project/Program.cs
+++ end_project/Program.cs
@@ -1,3 +1,9 @@
 ﻿int num = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[num];
 List<string> arrayNew = new List<string>();
+for (int i = 0; i < num; i++)
+{
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3) arrayNew.Add(array[i]);
+}
