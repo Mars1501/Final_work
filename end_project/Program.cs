c5081150ac06f7418de522df066dd8d97e8e80af
Denis <den.batuev99@gmail.com>
2022-08-09 23:23:48 +0300

    third commit

--- end_project/Program.cs
+++ end_project/Program.cs
@@ -7,3 +7,9 @@ for (int i = 0; i < num; i++)
     array[i] = Console.ReadLine();
     if (array[i].Length <= 3) arrayNew.Add(array[i]);
 }
+if (arrayNew.Count > 0)
+{
+    Console.WriteLine("Массив из элементов, длина которых <=3 символов: ");
+    foreach (string value in arrayNew) Console.WriteLine($"Это {arrayNew.IndexOf(value) + 1} элемент массива: {value}");
+}
+else Console.WriteLine("Не было ни одного элемента меньше 3х символов длиной.");
